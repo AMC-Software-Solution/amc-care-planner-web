81a137b897f3714f63ab8a346699485787cafe07
Khaled <qaalid037@gmail.com>
2021-04-20 17:12:57 +0300

    I have created tables both Notifications And Access Logs

--- src/container/employees/employee/access-logs/AccessLogs.js
+++ src/container/employees/employee/access-logs/AccessLogs.js
@@ -1,36 +1,146 @@
-import React from 'react';
-import { Row } from 'antd';
-import FeatherIcon from 'feather-icons-react';
-import { PageHeader } from '../../../../components/page-headers/page-headers';
-import { Button } from '../../../../components/buttons/buttons';
-import { Main } from '../../../styled';
-import { ExportButtonPageHeader } from '../../../../components/buttons/export-button/export-button';
-import { CalendarButtonPageHeader } from '../../../../components/buttons/calendar-button/calendar-button';
 
-const AccessLogs = () => {
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Table } from 'antd';
+import FeatherIcon from 'feather-icons-react';
+import { EmployeeTableStyleWrapper } from '../style';
+import { CardToolbox, TableWrapper } from '../../../styled';
+
+import { PageHeader } from '../../../../components/page-headers/page-headers';
+import { AutoComplete } from '../../../../components/autoComplete/autoComplete';
+
+import { Button } from '../../../../components/buttons/buttons';
+import { Cards } from '../../../../components/cards/frame/cards-frame';
+
+const AccessLogs = ({accessLogs}) => {
+  const accessLogsTableData = [];
+  if (accessLogs) {
+    accessLogs.map(accessLog => {
+      const {
+        id,
+        clientId,
+        keySafeNumber,
+        AccesDetails,
+        createdDate,
+        hasExtraData,
+        lastUpdatedDate,
+        
+
+        
+      } = accessLog;
+      return accessLogsTableData.push({
+        key: id,
+
+        clientId,
+        keySafeNumber,
+        AccesDetails,
+        createdDate,
+        hasExtraData,
+        lastUpdatedDate,
+        action: (
+          <div className="table-actions">
+            <>
+              <Button className="btn-icon" type="primary" shape="circle">
+                <Link to={`/admin/accesLogs/accessLog/accessLogProfile/${id}`}>
+                  <FeatherIcon icon="eye" size={16} />
+                </Link>
+              </Button>
+              <Button className="btn-icon" type="info" to="#" shape="circle">
+                <FeatherIcon icon="edit" size={16} />
+              </Button>
+              <Button className="btn-icon" type="danger" to="#" shape="circle">
+                <FeatherIcon icon="trash-2" size={16} />
+              </Button>
+            </>
+          </div>
+        ),
+      });
+    });
+  }
+
+  const accessLogsTableColumns = [
+   
+    {
+      title: 'Client ID',
+      dataIndex: 'clientId',
+      key: 'clientId',
+    },
+    {
+      title: 'key Safe Number',
+      dataIndex: 'keySafeNumber',
+      key: 'keySafeNumber',
+    },
+
+    {
+      title: 'Acces Details',
+      dataIndex: 'AccesDetails',
+      key: 'AccesDetails',
+    },
+    {
+     
+      title: 'Created Date',
+      dataIndex: 'createdDate',
+      key: 'createdDate',
+    },
+
+    {
+      title: 'HasExtra Data',
+      dataIndex: 'hasExtraData',
+      key: 'hasExtraData',
+    },
+
+    {
+      title: 'Last Updated Date',
+      dataIndex: 'lastUpdatedDate',
+      key: 'lastUpdatedDate',
+    },
+  ];
+
+  const rowSelection = {
+    getCheckboxProps: record => ({
+      disabled: record.name === 'Disabled AccessLog', // Column configuration not to be checked
+      name: record.name,
+    }),
+  };
+
   return (
     <>
-      <PageHeader
-        ghost
-        title="Access Logs"
-        buttons={[
-          <div key="1" className="page-header-actions">
-            <CalendarButtonPageHeader />
-            <ExportButtonPageHeader />
-            <Button size="small" type="primary">
-              <FeatherIcon icon="plus" size={14} />
-              Add New
-            </Button>
-          </div>,
-        ]}
-      />
-      <Main>
-        <Row justify="center" gutter={25}>
-          <div>WELCOME!</div>
-        </Row>
-      </Main>
+      <CardToolbox>
+        <PageHeader
+          ghost
+          title="Your Access Log"
+          subTitle={
+            <>
+              <span className="title-counter">{accessLogsTableData.length} Your AccessLogs</span>
+              <AutoComplete placeholder="Search by Name" width="100%" patterns />
+              <Button size="small" type="primary">
+                <FeatherIcon icon="plus" size={14} />
+                Create New Access Logs
+              </Button>
+          
+            </>
+          }
+        />
+      </CardToolbox>
+      <Cards headless>
+        <EmployeeTableStyleWrapper>
+          <TableWrapper className="table-responsive">
+            <Table
+              rowSelection={rowSelection}
+              dataSource={accessLogsTableData}
+              columns={accessLogsTableColumns}
+              pagination={{
+                defaultPageSize: 5,
+                total: accessLogsTableData.length,
+                showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} items`,
+              }}
+            />
+          </TableWrapper>
+        </EmployeeTableStyleWrapper>
+      </Cards>
     </>
   );
 };
+
 
 export default AccessLogs;
--- src/container/employees/employee/notification/Notifications.js
+++ src/container/employees/employee/notification/Notifications.js
@@ -1,34 +1,164 @@
-import React from 'react';
-import { Row } from 'antd';
-import FeatherIcon from 'feather-icons-react';
-import { PageHeader } from '../../../../components/page-headers/page-headers';
-import { Button } from '../../../../components/buttons/buttons';
-import { Main } from '../../../styled';
-import { ExportButtonPageHeader } from '../../../../components/buttons/export-button/export-button';
-import { CalendarButtonPageHeader } from '../../../../components/buttons/calendar-button/calendar-button';
 
-const Notifications = () => {
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Table } from 'antd';
+import FeatherIcon from 'feather-icons-react';
+import { EmployeeTableStyleWrapper } from '../style';
+import { CardToolbox, TableWrapper } from '../../../styled';
+
+import { PageHeader } from '../../../../components/page-headers/page-headers';
+import { AutoComplete } from '../../../../components/autoComplete/autoComplete';
+
+import { Button } from '../../../../components/buttons/buttons';
+import { Cards } from '../../../../components/cards/frame/cards-frame';
+
+const Notifications = ({notifications}) => {
+  const notificationsTableData = [];
+  if (notifications) {
+    notifications.map(notification => {
+      const {
+        id,
+        clientId,
+        title,
+        body,
+        notificationDate,
+        senderId,
+        recieverId,
+        createdDate,
+        hasExtraData,
+        lastUpdatedDate,
+        
+
+        
+      } = notification;
+      return notificationsTableData.push({
+        key: id,
+
+        id,
+        clientId,
+        title,
+        body,
+        notificationDate,
+        senderId,
+        recieverId,
+        createdDate,
+        hasExtraData,
+        lastUpdatedDate,
+        action: (
+          <div className="table-actions">
+            <>
+              <Button className="btn-icon" type="primary" shape="circle">
+                <Link to={`/admin/communications/communication/communicationProfile/${id}`}>
+                  <FeatherIcon icon="eye" size={16} />
+                </Link>
+              </Button>
+              <Button className="btn-icon" type="info" to="#" shape="circle">
+                <FeatherIcon icon="edit" size={16} />
+              </Button>
+              <Button className="btn-icon" type="danger" to="#" shape="circle">
+                <FeatherIcon icon="trash-2" size={16} />
+              </Button>
+            </>
+          </div>
+        ),
+      });
+    });
+  }
+
+  const notificationsTableColumns = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: 'Client ID',
+      dataIndex: 'clientId',
+      key: 'clientId',
+    },
+    {
+      title: 'body',
+      dataIndex: 'body',
+      key: 'body',
+    },
+
+    {
+      title: 'notification Date',
+      dataIndex: 'notificationDate',
+      key: 'notificationDate',
+    },
+    {
+      title: 'sender Id',
+      dataIndex: 'senderId',
+      key: 'senderId',
+    },
+    {
+      title: 'reciever Id', 
+      dataIndex: 'recieverId',
+      key: 'recieverId',
+    },
+    {
+     
+      title: 'Created Date',
+      dataIndex: 'createdDate',
+      key: 'createdDate',
+    },
+
+    {
+      title: 'HasExtra Data',
+      dataIndex: 'hasExtraData',
+      key: 'hasExtraData',
+    },
+
+    {
+      title: 'Last Updated Date',
+      dataIndex: 'lastUpdatedDate',
+      key: 'lastUpdatedDate',
+    },
+  ];
+
+  const rowSelection = {
+    getCheckboxProps: record => ({
+      disabled: record.name === 'Disabled Communication', // Column configuration not to be checked
+      name: record.name,
+    }),
+  };
+
   return (
     <>
-      <PageHeader
-        ghost
-        title="Notifications"
-        buttons={[
-          <div key="1" className="page-header-actions">
-            <CalendarButtonPageHeader />
-            <ExportButtonPageHeader />
-            <Button size="small" type="primary">
-              <FeatherIcon icon="plus" size={14} />
-              Add New
-            </Button>
-          </div>,
-        ]}
-      />
-      <Main>
-        <Row justify="center" gutter={25}>
-          <div>WELCOME!</div>
-        </Row>
-      </Main>
+      <CardToolbox>
+        <PageHeader
+          ghost
+          title="Your Notifications"
+          subTitle={
+            <>
+              <span className="title-counter">{notificationsTableData.length} Your Notifications</span>
+              <AutoComplete placeholder="Search by Name" width="100%" patterns />
+              <Button size="small" type="primary">
+                <FeatherIcon icon="plus" size={14} />
+                Create New Notifications
+              </Button>
+          
+            </>
+          }
+        />
+      </CardToolbox>
+      <Cards headless>
+        <EmployeeTableStyleWrapper>
+          <TableWrapper className="table-responsive">
+            <Table
+              rowSelection={rowSelection}
+              dataSource={notificationsTableData}
+              columns={notificationsTableColumns}
+              pagination={{
+                defaultPageSize: 5,
+                total: notificationsTableData.length,
+                showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} items`,
+              }}
+            />
+          </TableWrapper>
+        </EmployeeTableStyleWrapper>
+      </Cards>
     </>
   );
 };
